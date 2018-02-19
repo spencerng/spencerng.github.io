ca663d6ee50291fddfa5291e70a8de89a74525ed
spencerng <sng1488@gmail.com>
2018-02-18 23:50:11 -0500

    Change redirect to download app

--- js/scoutingapp_redirect.js
+++ js/scoutingapp_redirect.js
@@ -4,10 +4,10 @@ function callGitHubAPI(){
 	request.onreadystatechange = function(){
 		var response = JSON.parse(request.responseText);
 	 	
-	 	window.location.href = response[0].html_url;
+	 	window.location.href = response.assets[0].browser_download_url;
 
 	}
-	request.open('get', 'https://api.github.com/repos/spencerng/scouting-app/releases', true)
+	request.open('get', 'https://api.github.com/repos/spencerng/scouting-app/releases/latest', true)
 	request.send()
 }
 
