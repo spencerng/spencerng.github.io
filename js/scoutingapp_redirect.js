bdd32b3cc06b8f877d624c9c555b0a152f1a72c1
spencerng <sng1488@gmail.com>
2018-02-19 00:02:13 -0500

    Automatically close window in scouting app redirect

--- js/scoutingapp_redirect.js
+++ js/scoutingapp_redirect.js
@@ -5,6 +5,7 @@ function callGitHubAPI(){
 		var response = JSON.parse(request.responseText);
 	 	
 	 	window.location.href = response.assets[0].browser_download_url;
+	 	setTimeout(function(){window.close()}, 200);
 
 	}
 	request.open('get', 'https://api.github.com/repos/spencerng/scouting-app/releases/latest', true)
