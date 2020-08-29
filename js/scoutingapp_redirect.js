19982fa5fcab3a6f32b9ab6295d7114d3ca15674
spencerng <sng1488@gmail.com>
2020-08-29 13:59:51 -0400

    Update bio info

--- js/scoutingapp_redirect.js
+++ js/scoutingapp_redirect.js
@@ -3,9 +3,9 @@ function callGitHubAPI(){
 	
 	request.onreadystatechange = function(){
 		var response = JSON.parse(request.responseText);
-	 	
+	 	var type = response.assets[0].content_type
 	 	window.location.href = response.assets[0].browser_download_url;
-	 	setTimeout(function(){window.close()}, 200);
+	 	setTimeout(function(){window.close()}, 500);
 
 	}
 	request.open('get', 'https://api.github.com/repos/RaiderRobotix/scouting-app/releases/latest', true)
